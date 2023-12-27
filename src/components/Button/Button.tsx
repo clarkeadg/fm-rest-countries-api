6a779a3fb26bd7b95004bd7d42479927617dda21
clarkeadg <clarkeadg@yahoo.com>
2023-12-27 12:44:07 -0800

    button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,7 +9,7 @@ type ButtonProps = {
 
 const Button = ({ handleClick=()=>{}, accessoryLeft, accessoryRight, children }:ButtonProps) => {
   return (
-    <button onClick={handleClick} className={"button flex items-center justify-between px-4 py-3"}>
+    <button onClick={handleClick} className={"button flex items-center justify-between px-7 md:px-10 py-2 gap-3 text-[14px] md:text-[16px] rounded shadow hover:opacity-90"}>
       { accessoryLeft && accessoryLeft }
       { children && children }
       { accessoryRight && accessoryRight }
--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,10 +1,16 @@
 import Layout from '../components/Layout/Layout'
+import Button from '../components/Button/Button'
+import { FaArrowLeftLong } from "react-icons/fa6";
 
 const Details = () => {
   return (
     <Layout>
-      <section className="px-4 md:px-[80px] py-5">
-        Details Page
+      <section className="px-8 md:px-[80px] pt-[40px] md:pt-[82px]">
+        <div className="flex w-full items-center justify-between">
+          <Button accessoryLeft={<FaArrowLeftLong />}>
+            Back
+          </Button>
+        </div>
       </section>
     </Layout>    
   )
