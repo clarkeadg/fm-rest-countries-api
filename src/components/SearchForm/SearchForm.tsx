cdc59033594e706e7724bf2ea408e974686cf9bd
clarkeadg <clarkeadg@yahoo.com>
2023-12-27 14:19:38 -0800

    search form styles

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -14,16 +14,14 @@ const SearchForm = ({  }:SearchFormProps) => {
   }
   
   return (
-    <div className={"search-form"}>
-      <form onSubmit={handleSubmit} data-testid="search-form" className="flex items-center gap-2 md:gap-4">
-        <div>
-          <FaSearch/>
-        </div>
+    <div className={"search-form px-10 md:px-9 rounded shadow w-[480px] max-w-full mb-11 md:mb-0"}>
+      <form onSubmit={handleSubmit} data-testid="search-form" className="flex items-center gap-6 md:gap-4">        
+        <FaSearch className="text-[14px] md:text-[17px]"/>        
         <div className="flex grow">
           <input
             value={query}
             onChange={(e)=>{ setQuery(e.target.value)}}
-            className="text-xs md:text-[18px] leading-none px-2 py-4 md:py-5 w-full focus:outline-none mt-[2px]"
+            className="text-[12px] md:text-[15px] leading-none px-2 py-4 md:py-5 w-full focus:outline-none mt-[2px]"
             type="text"
             placeholder="Search for a country..."
             data-testid="search-input"
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,8 @@ import Link from '../lib/Link'
 
 const Home = () => {
   return (
-    <section className="px-4 md:px-[80px] py-5">
-      <div className="flex w-full items-center justify-between"> 
+    <section className="px-4 md:px-[80px] pt-[25px] md:pt-[50px]">
+      <div className="md:flex w-full items-center justify-between"> 
         <SearchForm/>
         <Dropdown>
           Filter by Region
