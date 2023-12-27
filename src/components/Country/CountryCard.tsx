f2bf0d233be5ea9b2f1410899a4f5b71ee0daa1d
clarkeadg <clarkeadg@yahoo.com>
2023-12-27 14:40:22 -0800

    styles

--- src/components/Country/CountryCard.tsx
+++ src/components/Country/CountryCard.tsx
@@ -2,13 +2,35 @@ import { memo } from 'react'
 import './Country.css'
 
 type CountryCardProps = {
-
+  title: string,
+  population: string,
+  region: string,
+  capital: string,
 }
 
-const CountryCard = ({  }:CountryCardProps) => {
+const CountryCard = ({ title, population, region, capital }:CountryCardProps) => {
   return (
-    <div className={"country-card"}>
-      Country Card
+    <div className="country-card rounded-md overflow-hidden shadow w-[265px] max-w-full hover:opacity-90">
+      <div className="w-full h-[160px] bg-red-200">
+
+      </div>
+      <div className="p-7">
+        <div className="text-[18px] font-bold mb-4">{ title }</div>
+        <div className="text-[13px] leading-[24px] mb-4">
+          <div className="">
+            <span className="font-bold">Population: </span>
+            { population }
+          </div>
+          <div className="">
+            <span className="font-bold">Region: </span>
+            { region }
+          </div>
+          <div className="">
+            <span className="font-bold">Capital: </span>
+            { capital }
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,15 +6,20 @@ import Link from '../lib/Link'
 const Home = () => {
   return (
     <section className="px-4 md:px-[80px] pt-[25px] md:pt-[50px]">
-      <div className="md:flex w-full items-center justify-between"> 
+      <div className="md:flex w-full items-center justify-between mb-10"> 
         <SearchForm/>
         <Dropdown>
           Filter by Region
         </Dropdown> 
       </div>
-      <div className="py-10"> 
+      <div className="flex justify-center md:justify-start py-3"> 
         <Link href="/details">
-          <CountryCard/>
+          <CountryCard
+            title={"Germany"}
+            population={"81,770,900"}
+            region={"Europe"}
+            capital={"Berlin"}
+          />
         </Link>        
       </div>
     </section>   
